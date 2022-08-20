dcd34cf31de11f8f237514b1dc9201c28279959a
Dmitry Babenko <dmitry@ergoserv.com>
2022-08-20 13:14:34 +0300

    Initial commit

---
+++ lib/auxiliary_rails_resourceable.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "auxiliary_rails_resourceable/version"
+
+module AuxiliaryRailsResourceable
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/auxiliary_rails_resourceable/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module AuxiliaryRailsResourceable
+  VERSION = "0.1.0"
+end
